4f30f16b36537fdfd152e8ea34fa25f45240727a
Anssi Kinnunen <anssi.kinnunen@gmail.com>
2013-04-30 12:11:11 +0300

    Added metadata files for rubygems and bundler.

---
+++ opsworks_bundler/metadata.rb
@@ -0,0 +1,12 @@
+maintainer        "Opscode, Inc."
+maintainer_email  "cookbooks@opscode.com"
+license           "Apache 2.0"
+description       "Installs and configures bundler"
+version           "0.8"
+
+%w{ ubuntu debian }.each do |os|
+  supports os
+end
+
+# depends 'opsworks_rubygems'
+# depends 'opsworks_bundler'
