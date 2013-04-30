695555d83324253dff31b7f5019c18443f3493b7
Anssi Kinnunen <anssi.kinnunen@gmail.com>
2013-04-30 13:12:27 +0300

    Added metadata for opsworks_commons. Added missing deps for ruby.

--- opsworks_commons/metadata.rb
+++ opsworks_commons/metadata.rb
@@ -1,14 +1,9 @@
 maintainer        "Opscode, Inc."
 maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
-description       "Installs and configures ruby"
+description       "Opsworks commons"
 version           "0.8"
 
 %w{ ubuntu debian }.each do |os|
   supports os
 end
-
-depends 'opsworks_initial_setup'
-depends 'opsworks_commons'
-depends 'opsworks_rubygems'
-depends 'opsworks_bundler'
