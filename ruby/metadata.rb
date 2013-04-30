faaa9814412b983ce71686661b5efddebb3b6362
Anssi Kinnunen <anssi.kinnunen@gmail.com>
2013-04-30 11:51:35 +0300

    Added metadata file for ruby.

---
+++ ruby/metadata.rb
@@ -0,0 +1,9 @@
+maintainer        "Opscode, Inc."
+maintainer_email  "cookbooks@opscode.com"
+license           "Apache 2.0"
+description       "Installs and configures ruby"
+version           "0.8"
+
+%w{ ubuntu debian }.each do |os|
+  supports os
+end
