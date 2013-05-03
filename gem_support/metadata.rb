91d67945f9e74647fd367e69ba2a20361cdcf047
Anssi Kinnunen <anssi.kinnunen@gmail.com>
2013-05-03 14:26:42 +0300

    Added dummy attribute for Metadata.

--- gem_support/metadata.rb
+++ gem_support/metadata.rb
@@ -1,3 +1,8 @@
 maintainer        "Amazon Web Services"
-description       "Offers a few library functions to simplify dealing with RubyGems"
+description       "Offers a few library functions to simplify dealing with Metadata"
 version           "0.1"
+
+attribute "metadata",
+  :display_name => "Metadata",
+  :description => "Hash of Metadata attributes",
+  :type => "hash"
