2681509bae77266f79e6fddefe566b31023b5c2c
Anssi Kinnunen <anssi.kinnunen@gmail.com>
2013-05-03 15:03:25 +0300

    Debugging dependencies metadata.rb.

--- dependencies/metadata.rb
+++ dependencies/metadata.rb
@@ -7,28 +7,28 @@ attribute "dependencies/gems",
   :display_name => "Gems to install",
   :description => "A list of Rubygems to install",
   :required => false,
-  :type => "hash"
+  :type => 'hash'
 
 attribute "dependencies/debs",
   :display_name => "Debian packages to install",
   :description => "A list of Debian packages (.deb) to install",
   :required => false,
-  :type => "hash"
+  :type => 'hash'
 
 attribute "dependencies/update_debs",
   :display_name => "Update sources",
   :description => "Update sources using apt-get update",
   :required => false,
-  :type => "boolean"
+  :type => 'hash'
 
 attribute "dependencies/upgrade_debs",
   :display_name => "Update packages",
   :description => "Update packages using apt-get upgrade",
   :required => false,
-  :type => "boolean"
+  :type => 'hash'
 
 attribute "dependencies/upgrade_gems",
   :display_name => "Update gems",
   :description => "Update gems using gem update",
   :required => false,
-  :type => "boolean"
+  :type => 'hash'
